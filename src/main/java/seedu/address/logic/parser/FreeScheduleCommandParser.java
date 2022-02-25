f332a7a00d7efbb40963e126b632efc6a6034689
hanqinilnix <ohanqin@gmail.com>
2022-02-25 10:48:05 +0800

    Add FreeScheduleCommandParser to handle user input

---
+++ src/main/java/seedu/address/logic/parser/FreeScheduleCommandParser.java
@@ -0,0 +1,38 @@
+package seedu.address.logic.parser;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_DATE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TIME;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import seedu.address.logic.commands.schedule.FreeScheduleCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.schedule.EventCollideWithTimeDatePredicate;
+
+public class FreeScheduleCommandParser implements Parser<FreeScheduleCommand> {
+    @Override
+    public FreeScheduleCommand parse(String userInput) throws ParseException {
+        requireNonNull(userInput);
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(userInput, PREFIX_TIME, PREFIX_DATE);
+
+        boolean hasTimePrefix = argMultimap.getValue(PREFIX_TIME).isPresent();
+        boolean hasDatePrefix = argMultimap.getValue(PREFIX_DATE).isPresent();
+
+        if (hasTimePrefix && hasDatePrefix) {
+            LocalDate date = ParserUtil.parseDate(argMultimap.getValue(PREFIX_DATE).get());
+            LocalTime time = ParserUtil.parseTime(argMultimap.getValue(PREFIX_TIME).get());
+
+            return new FreeScheduleCommand(new EventCollideWithTimeDatePredicate(time, date));
+        } else if (hasTimePrefix) {
+            LocalTime time = ParserUtil.parseTime(argMultimap.getValue(PREFIX_TIME).get());
+
+            return new FreeScheduleCommand(new EventCollideWithTimeDatePredicate(time, LocalDate.now()));
+        } else {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, FreeScheduleCommand.MESSAGE_USAGE));
+        }
+    }
+}
---
+++ src/main/java/seedu/address/model/schedule/EventCollideWithTimeDatePredicate.java
@@ -0,0 +1,45 @@
+package seedu.address.model.schedule;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
+import seedu.address.model.person.Person;
+
+public class EventCollideWithTimeDatePredicate implements Predicate<Person> {
+
+    private final LocalTime time;
+    private final LocalDate date;
+
+    public EventCollideWithTimeDatePredicate(LocalTime time, LocalDate date) {
+        this.time = time;
+        this.date = date;
+    }
+
+    @Override
+    public boolean test(Person person) {
+        Schedule schedule = person.getSchedule();
+        List<Event> events = schedule.getEvents();
+        if (events.size() == 0) {
+            return false;
+        }
+        Stream<Event> eventsStream = events.stream();
+        return eventsStream
+                .filter(event -> event.getDate().equals(date))
+                .allMatch(event -> {
+                    LocalTime start = event.getTime();
+                    LocalTime end = start.plus(event.getDuration());
+
+                    return !(start.equals(time) || start.isBefore(time) && end.isAfter(time));
+                });
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof EventCollideWithTimeDatePredicate // instanceof handles nulls
+                && time.equals(((EventCollideWithTimeDatePredicate) other).time)); // state check
+    }
+}
