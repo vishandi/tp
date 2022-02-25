84818bf98137f9475bf9f4c3fce4ad9eb089030b
hanqinilnix <ohanqin@gmail.com>
2022-02-25 10:48:44 +0800

    Modify FreeScheduleCommand to display what is required

--- src/main/java/seedu/address/logic/commands/schedule/FreeScheduleCommand.java
+++ src/main/java/seedu/address/logic/commands/schedule/FreeScheduleCommand.java
@@ -1,14 +1,16 @@
 package seedu.address.logic.commands.schedule;
 
+import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_DATE;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TIME;
 
+import seedu.address.commons.core.Messages;
 import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.CommandResult;
+import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
+import seedu.address.model.schedule.EventCollideWithTimeDatePredicate;
 
 public class FreeScheduleCommand extends Command {
 
@@ -24,8 +26,24 @@ public class FreeScheduleCommand extends Command {
             + PREFIX_TIME + "12:00 "
             + PREFIX_DATE + "2022-02-14";
 
+    private final EventCollideWithTimeDatePredicate predicate;
+
+    public FreeScheduleCommand(EventCollideWithTimeDatePredicate predicate) {
+        this.predicate = predicate;
+    }
+
     @Override
     public CommandResult execute(Model model) throws CommandException {
-        return new CommandResult("Hello from freeSchedule");
+        requireNonNull(model);
+        model.updateFilteredPersonList(predicate);
+        return new CommandResult(
+                String.format(Messages.MESSAGE_PERSONS_LISTED_OVERVIEW, model.getFilteredPersonList().size()));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof FreeScheduleCommand // instanceof handles nulls
+                && predicate.equals(((FreeScheduleCommand) other).predicate)); // state check
     }
 }
