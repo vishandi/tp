02f8b338128e5dbebaadf8e1697892f41870f570
hanqinilnix <ohanqin@gmail.com>
2022-03-05 16:26:56 +0800

    Add EMPTY_SCHEDULE for easier use to create new test cases

--- src/main/java/seedu/address/model/schedule/Schedule.java
+++ src/main/java/seedu/address/model/schedule/Schedule.java
@@ -13,6 +13,7 @@ public class Schedule {
     public static final String MESSAGE_CONSTRAINTS =
             "A Schedule's Events must have alphanumeric event descriptions, date formats YYYY-MM-DD, "
                     + "time formats HH:MM and duration format in hours";
+    public static final Schedule EMPTY_SCHEDULE = new Schedule(new ArrayList<>());
 
     private final List<Event> events;
 
