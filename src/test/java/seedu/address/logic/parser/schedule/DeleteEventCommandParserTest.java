b78432c97a0c92c71269613afadb2a6a32aac60c
hanqinilnix <ohanqin@gmail.com>
2022-03-16 18:59:39 +0800

    Remove unused imports

--- src/test/java/seedu/address/logic/parser/schedule/DeleteEventCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/schedule/DeleteEventCommandParserTest.java
@@ -6,7 +6,6 @@ import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailur
 import org.junit.jupiter.api.Test;
 
 import seedu.address.logic.commands.schedule.DeleteEventCommand;
-import seedu.address.logic.parser.schedule.DeleteEventCommandParser;
 
 public class DeleteEventCommandParserTest {
 
