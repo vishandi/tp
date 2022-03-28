ee0eb12cdb4ca79388d2e6aa18c09d668194b423
tyanhan <tayyanhanw@gmail.com>
2022-03-28 16:43:09 +0800

    Fix checkstyle

--- src/test/java/seedu/address/logic/parser/schedule/ClearScheduleCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/schedule/ClearScheduleCommandParserTest.java
@@ -20,6 +20,7 @@ public class ClearScheduleCommandParserTest {
 
     @Test
     public void parse_invalidArgs_throwsParseException() {
-        assertParseFailure(parser, "a", String.format(MESSAGE_INVALID_COMMAND_FORMAT, ClearScheduleCommand.MESSAGE_USAGE));
+        assertParseFailure(
+                parser, "a", String.format(MESSAGE_INVALID_COMMAND_FORMAT, ClearScheduleCommand.MESSAGE_USAGE));
     }
 }
