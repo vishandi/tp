591dbc6ca63800d2938de47a53021eac4363e1fd
vishandi <vishandirkeneta@gmail.com>
2022-03-28 21:32:38 +0800

    Remove clickable github and email

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -103,62 +103,6 @@ public class PersonCard extends UiPart<Region> {
                 .forEach(tag -> tags.getChildren().add(new Label(tag.tagName)));
     }
 
-    @FXML
-    private void emailtoContact() {
-        if (Desktop.isDesktopSupported()) {
-            Desktop desktop = Desktop.getDesktop();
-            URI mailto = null;
-            try {
-                mailto = new URI(String.format("mailto:%s", person.getEmail()));
-            } catch (URISyntaxException e) {
-                e.printStackTrace();
-            }
-            try {
-                desktop.mail(mailto);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    @FXML
-    private void openGithub() {
-        if (Desktop.isDesktopSupported()) {
-            Desktop desktop = Desktop.getDesktop();
-            URI githubUri = null;
-            try {
-                githubUri = new URI("https://github.com/" + person.getGithub());
-            } catch (URISyntaxException e) {
-                e.printStackTrace();
-            }
-            try {
-                desktop.browse(githubUri);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    @FXML
-    private void changeEmailColorWhenHovered() {
-        emailBox.setStyle("-fx-background-color: #c66c6c;");
-    }
-
-    @FXML
-    private void changeEmailColorWhenLeft() {
-        emailBox.setStyle("-fx-background-color: transparent;");
-    }
-
-    @FXML
-    private void changeGithubColorWhenHovered() {
-        githubBox.setStyle("-fx-background-color: #c66c6c;");
-    }
-
-    @FXML
-    private void changeGithubColorWhenLeft() {
-        githubBox.setStyle("-fx-background-color: transparent;");
-    }
-
     @Override
     public boolean equals(Object other) {
         // short circuit if same object
