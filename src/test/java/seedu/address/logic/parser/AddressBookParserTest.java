f3ba5938387b8cd7912e4f7b880b35d6e3a4e47b
vishandi <vishandirkeneta@gmail.com>
2022-03-28 15:01:38 +0800

    Add case insensitive test

--- src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
@@ -51,7 +51,24 @@ import seedu.address.testutil.PersonUtil;
 
 public class AddressBookParserTest {
 
+    private static final String ADD_COMMAND_UPPER = "ADD";
+    private static final String CLEAR_COMMAND_UPPER = "CLEAR";
+    private static final String DELETE_COMMAND_UPPER = "DELETE";
+    private static final String EDIT_COMMAND_UPPER = "EDIT";
+    private static final String EXIT_COMMAND_UPPER = "EXIT";
+    private static final String FIND_COMMAND_UPPER = "FIND";
+    private static final String HELP_COMMAND_UPPER = "HELP";
+    private static final String LIST_COMMAND_UPPER = "LIST";
+    private static final String ADD_EVENT_COMMAND_UPPER = "ADDEVENT";
+    private static final String EDIT_EVENT_COMMAND_UPPER = "EDITEVENT";
+    private static final String DELETE_EVENT_COMMAND_UPPER = "DELETEEVENT";
+    private static final String FREE_SCHEDULE_COMMAND_UPPER = "FREESCHEDULE";
+    private static final String VIEW_GROUP_COMMAND_UPPER = "VIEWGROUP";
+    private static final String VIEW_SCHEDULE_COMMAND_UPPER = "VIEWSCHEDULE";
+    private static final String EXPORT_COMMAND_UPPER = "EXPORT";
+
     private final AddressBookParser parser = new AddressBookParser();
+
 
     @Test
     public void parseCommand_add() throws Exception {
@@ -166,6 +183,141 @@ public class AddressBookParserTest {
                 ExportCommand.COMMAND_WORD + " " + INDEX_FIRST_PERSON.getOneBased()
         );
         assertEquals(new ExportCommand(INDEX_FIRST_PERSON), command);
+    }
+
+    @Test
+    public void parseCommand_allLowerCase_success() throws ParseException {
+        // addEvent
+        Event event = new EventBuilder().build();
+        AddEventCommand addEventCommand = (AddEventCommand) parser
+                .parseCommand(AddEventCommand.COMMAND_WORD_LOWER
+                        + PersonUtil.getAddEventCommand(INDEX_FIRST_PERSON, event).substring(8));
+        assertEquals(new AddEventCommand(INDEX_FIRST_PERSON, event), addEventCommand);
+
+        // editEvent
+        EditEventDescriptor descriptor = new EditEventDescriptorBuilder(event).build();
+        EditEventCommand editEventCommand = (EditEventCommand) parser.parseCommand(EditEventCommand.COMMAND_WORD_LOWER
+                + " " + INDEX_FIRST_PERSON.getOneBased() + " " + INDEX_FIRST_EVENT.getOneBased()
+                + " " + PersonUtil.getEditEventDescriptorDetails(descriptor));
+        assertEquals(new EditEventCommand(INDEX_FIRST_PERSON, INDEX_FIRST_EVENT, descriptor), editEventCommand);
+
+        // deleteEvent
+        DeleteEventCommand deleteEventCommand = (DeleteEventCommand) parser.parseCommand(
+                DeleteEventCommand.COMMAND_WORD_LOWER + " " + INDEX_FIRST_PERSON.getOneBased()
+                        + " " + INDEX_FIRST_EVENT.getOneBased());
+        assertEquals(new DeleteEventCommand(INDEX_FIRST_PERSON, INDEX_FIRST_EVENT), deleteEventCommand);
+
+        // freeSchedule
+        FreeScheduleCommand freeScheduleCommand = (FreeScheduleCommand) parser.parseCommand(
+                FreeScheduleCommand.COMMAND_WORD_LOWER + " " + PREFIX_TIME + TYPICAL_TIME.toString() + " "
+                        + PREFIX_DATE + TYPICAL_DATE.toString()
+        );
+        assertEquals(new FreeScheduleCommand(new IsPersonFreePredicate(TYPICAL_TIME, TYPICAL_DATE)),
+                freeScheduleCommand);
+
+        // viewGroup
+        String keyword = "foo";
+        ViewGroupCommand viewGroupCommand = (ViewGroupCommand) parser.parseCommand(
+                ViewGroupCommand.COMMAND_WORD_LOWER + " " + PREFIX_TAG + keyword
+        );
+        assertEquals(new ViewGroupCommand(new IsTagInPersonPredicate(new Tag(keyword))), viewGroupCommand);
+
+        // viewSchedule
+        ViewScheduleCommand command = (ViewScheduleCommand) parser.parseCommand(
+                ViewScheduleCommand.COMMAND_WORD_LOWER + " " + INDEX_FIRST_PERSON.getOneBased()
+        );
+        assertEquals(new ViewScheduleCommand(INDEX_FIRST_PERSON), command);
+    }
+
+    @Test
+    public void parseCommand_allUpperCase_success() throws ParseException {
+        // add
+        Person person = new PersonBuilder().build();
+        AddCommand addCommand = (AddCommand) parser.parseCommand( ADD_COMMAND_UPPER
+                + PersonUtil.getAddCommand(person).substring(3));
+        assertEquals(new AddCommand(person), addCommand);
+
+        // clear
+        assertTrue(parser.parseCommand(CLEAR_COMMAND_UPPER) instanceof ClearCommand);
+        assertTrue(parser.parseCommand(CLEAR_COMMAND_UPPER + " 3") instanceof ClearCommand);
+
+        // delete
+        DeleteCommand deleteCommand = (DeleteCommand) parser.parseCommand(
+                DELETE_COMMAND_UPPER + " " + INDEX_FIRST_PERSON.getOneBased());
+        assertEquals(new DeleteCommand(INDEX_FIRST_PERSON), deleteCommand);
+
+        // edit
+        person = new PersonBuilder().build();
+        EditPersonDescriptor descriptor = new EditPersonDescriptorBuilder(person).build();
+        EditCommand editCommand = (EditCommand) parser.parseCommand(EDIT_COMMAND_UPPER + " "
+                + INDEX_FIRST_PERSON.getOneBased() + " " + PersonUtil.getEditPersonDescriptorDetails(descriptor));
+        assertEquals(new EditCommand(INDEX_FIRST_PERSON, descriptor), editCommand);
+
+        // exit
+        assertTrue(parser.parseCommand(EXIT_COMMAND_UPPER) instanceof ExitCommand);
+        assertTrue(parser.parseCommand(EXIT_COMMAND_UPPER + " 3") instanceof ExitCommand);
+
+        // find
+        List<String> keywords = Arrays.asList("foo", "bar", "baz");
+        FindCommand findCommand = (FindCommand) parser.parseCommand(
+                FIND_COMMAND_UPPER + " " + keywords.stream().collect(Collectors.joining(" ")));
+        assertEquals(new FindCommand(new NameContainsKeywordsPredicate(keywords)), findCommand);
+
+        // help
+        assertTrue(parser.parseCommand(HELP_COMMAND_UPPER) instanceof HelpCommand);
+        assertTrue(parser.parseCommand(HELP_COMMAND_UPPER + " 3") instanceof HelpCommand);
+
+        // list
+        assertTrue(parser.parseCommand(LIST_COMMAND_UPPER) instanceof ListCommand);
+        assertTrue(parser.parseCommand(LIST_COMMAND_UPPER + " 3") instanceof ListCommand);
+
+        // addEvent
+        Event event = new EventBuilder().build();
+        AddEventCommand addEventCommand = (AddEventCommand) parser
+                .parseCommand(ADD_EVENT_COMMAND_UPPER
+                        + PersonUtil.getAddEventCommand(INDEX_FIRST_PERSON, event).substring(8));
+        assertEquals(new AddEventCommand(INDEX_FIRST_PERSON, event), addEventCommand);
+
+        // editEvent
+        EditEventDescriptor editEventDescriptor = new EditEventDescriptorBuilder(event).build();
+        EditEventCommand editEventCommand = (EditEventCommand) parser.parseCommand(EDIT_EVENT_COMMAND_UPPER
+                + " " + INDEX_FIRST_PERSON.getOneBased() + " " + INDEX_FIRST_EVENT.getOneBased()
+                + " " + PersonUtil.getEditEventDescriptorDetails(editEventDescriptor));
+        assertEquals(new EditEventCommand(INDEX_FIRST_PERSON, INDEX_FIRST_EVENT, editEventDescriptor),
+                editEventCommand);
+
+        // deleteEvent
+        DeleteEventCommand deleteEventCommand = (DeleteEventCommand) parser.parseCommand(
+                DELETE_EVENT_COMMAND_UPPER + " " + INDEX_FIRST_PERSON.getOneBased()
+                        + " " + INDEX_FIRST_EVENT.getOneBased());
+        assertEquals(new DeleteEventCommand(INDEX_FIRST_PERSON, INDEX_FIRST_EVENT), deleteEventCommand);
+
+        // freeSchedule
+        FreeScheduleCommand freeScheduleCommand = (FreeScheduleCommand) parser.parseCommand(
+                FREE_SCHEDULE_COMMAND_UPPER + " " + PREFIX_TIME + TYPICAL_TIME.toString() + " "
+                        + PREFIX_DATE + TYPICAL_DATE.toString()
+        );
+        assertEquals(new FreeScheduleCommand(new IsPersonFreePredicate(TYPICAL_TIME, TYPICAL_DATE)),
+                freeScheduleCommand);
+
+        // viewGroup
+        String keyword = "foo";
+        ViewGroupCommand viewGroupCommand = (ViewGroupCommand) parser.parseCommand(
+                VIEW_GROUP_COMMAND_UPPER + " " + PREFIX_TAG + keyword
+        );
+        assertEquals(new ViewGroupCommand(new IsTagInPersonPredicate(new Tag(keyword))), viewGroupCommand);
+
+        // viewSchedule
+        ViewScheduleCommand command = (ViewScheduleCommand) parser.parseCommand(
+                VIEW_SCHEDULE_COMMAND_UPPER + " " + INDEX_FIRST_PERSON.getOneBased()
+        );
+        assertEquals(new ViewScheduleCommand(INDEX_FIRST_PERSON), command);
+
+        // export
+        ExportCommand exportCommand = (ExportCommand) parser.parseCommand(
+                EXPORT_COMMAND_UPPER + " " + INDEX_FIRST_PERSON.getOneBased()
+        );
+        assertEquals(new ExportCommand(INDEX_FIRST_PERSON), exportCommand);
     }
 
     @Test
