8abacb97697d786e984c4e66ff9996ed3c4b4f07
markbiju <markbiju16@gmail.com>
2022-04-01 03:36:38 +0800

    Update FindCommonTimingCommand.java

--- src/main/java/seedu/address/logic/commands/schedule/FindCommonTimingCommand.java
+++ src/main/java/seedu/address/logic/commands/schedule/FindCommonTimingCommand.java
@@ -25,14 +25,14 @@ import seedu.address.model.schedule.Schedule;
  */
 public class FindCommonTimingCommand extends Command {
 
-    public static final String COMMAND_WORD_LOWER = "findcommontiming";
-    public static final String MESSAGE_USAGE = COMMAND_WORD_LOWER
+    public static final String COMMAND_WORD = "findcommontiming";
+    public static final String MESSAGE_USAGE = COMMAND_WORD
             + ": Retrieves common free timings of contacts "
             + "who share the same tag\n"
             + "Parameters: "
             + PREFIX_TAG + "TAG"
-            + " [" + PREFIX_DATE + "DATE]\n"
-            + "Example: " + COMMAND_WORD_LOWER
+            + PREFIX_DATE + "DATE\n"
+            + "Example: " + COMMAND_WORD
             + PREFIX_TAG + "friends "
             + PREFIX_DATE + "2022-02-14";
 
